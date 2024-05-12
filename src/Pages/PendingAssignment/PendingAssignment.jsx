2e3b3f6928469a9fa519a4c3e444d7aa00534ebb
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-05-12 20:05:24 +0600

    Added Pending Assignment Page

--- src/Pages/PendingAssignment/PendingAssignment.jsx
+++ src/Pages/PendingAssignment/PendingAssignment.jsx
@@ -1,6 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 const PendingAssignment = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    async function fetchAssignment() {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/findstatusassignment/pending`
+        );
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching assignment:", error);
+      }
+    }
+    fetchAssignment();
+  }, []);
+
   return (
    <div  className="pt-24 mb-24">
     <div className="lg:max-w-6xl mx-auto">
@@ -11,40 +29,29 @@ const PendingAssignment = () => {
         <table
           className="min-w-full text-start text-sm font-light text-surface dark:text-white">
           <thead
-            className="border-b border-neutral-200 font-medium dark:border-white/10">
+            className="border-b bg-[#003C43] border-neutral-200 font-medium dark:border-white/10">
             <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Role</th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-[#E3FEF7] uppercase tracking-wider">Assignment Title</th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-[#E3FEF7] uppercase tracking-wider">Assignment Marks</th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-[#E3FEF7] uppercase tracking-wider">Examinee Name</th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-[#E3FEF7] uppercase tracking-wider">Status</th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-[#E3FEF7] uppercase tracking-wider">Action</th>
         </tr>
           </thead>
           <tbody>
-          <tr>
-            <td className="px-6 py-4 whitespace-nowrap">Jane Doe</td>
-            <td className="px-6 py-4 whitespace-nowrap">jane@example.com</td>
-            <td className="px-6 py-4 whitespace-nowrap">Admin</td>
-            <td className="px-6 py-4 whitespace-nowrap">
-                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-                <button className="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Edit</button>
-                <button className="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">Delete</button>
-            </td>
-        </tr>
-        <tr>
-            <td className="px-6 py-4 whitespace-nowrap">Jane Doe</td>
-            <td className="px-6 py-4 whitespace-nowrap">jane@example.com</td>
-            <td className="px-6 py-4 whitespace-nowrap">Admin</td>
-            <td className="px-6 py-4 whitespace-nowrap">
-                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-                <button className="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Edit</button>
-                <button className="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">Delete</button>
-            </td>
-        </tr>
+          {
+            data.map(newData => <tr key={newData._id}>
+              <td className="px-6 py-4 whitespace-nowrap">{newData.assignmentTitle}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{newData.assignmentMarks}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{newData.userName}</td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                  <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">{newData.status}</span>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                  <button className="px-4 py-2 font-medium text-white bg-[#135D66] rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">give mark</button>
+              </td>
+          </tr>)
+          }
           
           </tbody>
         </table>
@@ -58,4 +65,6 @@ const PendingAssignment = () => {
 };
 
 export default PendingAssignment;
+
+
 
